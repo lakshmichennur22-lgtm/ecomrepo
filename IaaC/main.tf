fcca207a61d3aa1c95c60967fa5d7b37d572a843
lakshmi <lakshmichennur22@gmail.com>
2026-01-18 17:55:20 +0530

    commit3

--- IaaC/main.tf
+++ IaaC/main.tf
@@ -192,7 +192,7 @@ resource "aws_eks_node_group" "default" {
     max_size     = 2
   }
 
-  instance_types = ["t3.medium"]
+  instance_types = ["t3.micro"]
 
   depends_on = [
     aws_iam_role_policy_attachment.node_policies
