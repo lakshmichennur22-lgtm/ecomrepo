d278958758117548b5bac60af353d93bcb88f5b6
lakshmi <lakshmichennur22@gmail.com>
2026-01-20 17:23:18 +0530

    commit21

--- IaaC/main.tf
+++ IaaC/main.tf
@@ -119,14 +119,20 @@ resource "aws_security_group" "eks_sg" {
     protocol    = "tcp"
     cidr_blocks = ["10.0.0.0/16"]
   }
-
+# HTTP from anywhere (temporary for testing)
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
+  
   tags = local.tags
 }
 
